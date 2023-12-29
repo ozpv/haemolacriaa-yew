38869b2212dd77e5b814637a0162a832f23dc758
ozpv <39195175+ozpv@users.noreply.github.com>
2023-12-29 04:38:13 +0000

    update + better tailwind integration

--- src/app/comp/nav.rs
+++ src/app/comp/nav.rs
@@ -5,16 +5,18 @@ use crate::config;
 #[function_component(Nav)]
 pub fn nav() -> Html {
     html! {
-        <nav>
-            <ul>
-                {
-                    config::NAV_ITEMS.into_iter().map(|item| {
-                        html! {
-                            <li><a href={item.href}>{item.text}</a></li>
-                        }
-                    }).collect::<Html>()
-                }
-            </ul>
+        <nav class={classes!("bg-black")}>
+            <div class={classes!("max-w-7xl", "mx-auto", "border", "border-red-400",)}>
+                <ul class="">
+                    {
+                        config::NAV_ITEMS.into_iter().map(|item| {
+                            html! {
+                                <li><a href={item.href}>{item.text}</a></li>
+                            }
+                        }).collect::<Html>()
+                    }
+                </ul>
+            </div>
         </nav>
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.rs',
+    './src/*.rs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
+
