5116a52cb14e6393b472f41a3c5bfd7c3b529537
ozpv <39195175+ozpv@users.noreply.github.com>
2024-01-02 23:45:01 +0000

    update constants

--- frontend/src/config.rs
+++ frontend/src/config.rs
@@ -9,13 +9,13 @@ use crate::{
 /* info section */
 
 /* assumes you have only one, synchronized name */
-const USERNAME: &'static str = "haemolacriaa";
+const USERNAME: &str = "haemolacriaa";
 
 /* derived from the final id on each profile URL, make sure to
  * ignore tracking info; this is usually presented after the id 
  * with a '?'.
  */
-const SPOTIFY_ARTIST_ID: &'static str = "7xu9uzSauNTYNgHFSAmmi6";
+const SPOTIFY_ARTIST_ID: &str = "7xu9uzSauNTYNgHFSAmmi6";
 
 /* two url parameters: set the apple music region and id... */ 
 const APPLE_MUSIC_REGION: &str = "us";
@@ -31,17 +31,17 @@ const YOUTUBE_CHANNEL_ID: &str = "UCQDQqA9iaWtlNkwXiCQogYQ";
 
 /* navbar section */
 
-pub const NAV_ITEMS: &'static [&'static nav::NavItem] = &[ 
-    &nav::NavItem{route: Route::Home, text: "Home",},
-    &nav::NavItem{route: Route::About, text: "About",},
-    &nav::NavItem{route: Route::Donate, text: "Donate",},
+pub const NAV_ITEMS: [nav::NavItem; 3] = [ 
+    nav::NavItem{route: Route::Home, text: "Home",},
+    nav::NavItem{route: Route::About, text: "About",},
+    nav::NavItem{route: Route::Donate, text: "Donate",},
     //&nav::NavItem{route: Route::Privacy, text: "Privacy",},
 ];
 
 /* link list section */
 
-pub const STREAMINGINFO_ITEMS: &'static [&'static links::StreamingInfo] = &[
-    &links::StreamingInfo{service: links::StreamingService::Spotify, service_name: "Spotify", url: "spotify.com/"}
+pub const STREAMINGINFO_ITEMS: [links::StreamingInfo; 1] = [
+    links::StreamingInfo{service: links::StreamingService::Spotify, service_name: "Spotify", url: "spotify.com/"}
 ];
 
 /* footer section */
@@ -49,10 +49,11 @@ pub const STREAMINGINFO_ITEMS: &'static [&'static links::StreamingInfo] = &[
 /* note: for icons, you must also add the icon's name
  * into yew_icons' features inside Cargo.toml.
  */
-pub const SOCIALMEDIAINFO_ITEMS: &'static [&'static links::SocialMediaInfo] = &[
-    &links::SocialMediaInfo{ico: IconId::SimpleIconsApple, url: formatcp!("https://music.apple.com/{}/artist/{}", APPLE_MUSIC_REGION, APPLE_MUSIC_ID)},
-    &links::SocialMediaInfo{ico: IconId::SimpleIconsSoundcloud, url: formatcp!("https://soundcloud.com/{}", USERNAME)},
-    &links::SocialMediaInfo{ico: IconId::SimpleIconsYoutube, url: formatcp!("https://youtube.com/channel/{}", YOUTUBE_CHANNEL_ID)},
-    &links::SocialMediaInfo{ico: IconId::SimpleIconsInstagram, url: formatcp!("https://instagram.com/{}", USERNAME)},
-    &links::SocialMediaInfo{ico: IconId::SimpleIconsSpotify, url: formatcp!("https://open.spotify.com/artist/{}", SPOTIFY_ARTIST_ID)},
+
+pub const SOCIALMEDIAINFO_ITEMS: [links::SocialMediaInfo; 5] = [
+    links::SocialMediaInfo{ico: IconId::SimpleIconsApple, url: formatcp!("https://music.apple.com/{}/artist/{}", APPLE_MUSIC_REGION, APPLE_MUSIC_ID)},
+    links::SocialMediaInfo{ico: IconId::SimpleIconsSoundcloud, url: formatcp!("https://soundcloud.com/{}", USERNAME)},
+    links::SocialMediaInfo{ico: IconId::SimpleIconsYoutube, url: formatcp!("https://youtube.com/channel/{}", YOUTUBE_CHANNEL_ID)},
+    links::SocialMediaInfo{ico: IconId::SimpleIconsInstagram, url: formatcp!("https://instagram.com/{}", USERNAME)},
+    links::SocialMediaInfo{ico: IconId::SimpleIconsSpotify, url: formatcp!("https://open.spotify.com/artist/{}", SPOTIFY_ARTIST_ID)},
 ];
