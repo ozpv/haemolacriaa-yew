e20ad960035f38d3b12fa02879d303cf08339284
ozpv <39195175+ozpv@users.noreply.github.com>
2024-01-19 02:16:03 +0000

    connect server and frontend

---
+++ backend/src/api/hello.rs
@@ -0,0 +1,9 @@
+use actix_web::{
+    get,
+    web::Json,
+};
+
+#[get("/test")]
+pub async fn hello() -> Json<String> {
+    Json(String::from("under construction..."))
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,26 +1,32 @@
-use axum::{
-    routing::get,
-    response::IntoResponse,
-    http::StatusCode,
-    Router,
-    Json,
+mod api;
+
+use actix_web::{
+    web::scope,
+    App,
+    HttpServer,
 };
+use actix_web_lab::web::spa;
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/api/test", get(handler));
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8081").await.unwrap();
-    
-    axum::serve(listener, app).await.unwrap();
+use api::hello::hello;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .service(
+                scope("/api")
+                    .service(hello)
+            )
+            .service(
+                spa()
+                .index_file("./dist/index.html")
+                .static_resources_mount("/")
+                .static_resources_location("./dist/")
+                .finish()
+            )
+    })
+    .bind(("0.0.0.0", 80))?
+    .run()
+    .await
 }
 
-#[derive(serde::Serialize)]
-struct Message {
-    message: String,
-}
-
-async fn handler() -> impl IntoResponse {
-    (StatusCode::OK, Json(Message {
-        message: String::from("Works!"),
-    }))
-}
